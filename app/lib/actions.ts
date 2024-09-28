644784c90a80010686b35f52902439fcaf69817d
Ren Saito <ren.saito.dev@gmail.com>
2024-09-28 16:15:19 +1200

    fix ESlint warnings

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,7 +61,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch {
     // If a database error occurs, return a more specific error.
     return {
       message: 'Database Error: Failed to Create Invoice.',
@@ -100,7 +100,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
@@ -113,7 +113,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
     return { message: 'Deleted Invoice.' };
-  } catch (error) {
+  } catch {
     return { message: 'Database Error: Failed to Delete Invoice.' };
   }
 }
